4e39195f11c0c613237026d42d5a9de13a1a18f1
juliano lab pc <julianoferrarigianlupi@gmail.com>
2018-06-12 15:32:56 -0400

    echo fix on the unix one, still untested

--- demoTesterUnix.sh
+++ demoTesterUnix.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
-echo path of demo        error level (0 success) > testResults.txt
+#!/bin/bash
+echo "path of demo        error level (0 success) > testResults.txt"
 for cc3dproj in ./Demos/*/*.cc3d do (
 	sh ./runScript.sh -i %cc3dproj --noOutput -f 10000000000 --exitWhenDone &\
 	echo %cc3dproj      %$ >> testResults.txt
+)
+exit 
