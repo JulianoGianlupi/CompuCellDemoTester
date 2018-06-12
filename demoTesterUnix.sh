f36267b91560fc2ab7bbc011cf0fdd69f129a2f2
juliano lab pc <julianoferrarigianlupi@gmail.com>
2018-06-12 17:46:05 -0400

    directory restructuring. unix odifications
    unix script still doesn't work properly, almost

--- demoTesterUnix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "path of demo        error level (0 success) > testResults.txt"
-for cc3dproj in ./Demos/*/*.cc3d do (
-	sh ./runScript.sh -i %cc3dproj --noOutput -f 10000000000 --exitWhenDone &\
-	echo %cc3dproj      %$ >> testResults.txt
-)
-exit 
---
+++ unix/demoTesterUnix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "path of demo        error level (0 success)" > testResults.txt
+
+shopt -s globstar
+for cc3dproj in ./Demos/**/*.cc3d 
+	do
+		sh ./runScript.sh -i $cc3dproj --noOutput -f 10000000000 --exitWhenDone &\
+		echo $cc3dproj      %$ >> testResults.txt
+		done
+exit 
+
+
+
